d9bc8cc73b8f49ef79f66f6f9acb474a96f4f9c5
Pablo Vega Uceta <pablo.vega@scmspain.com>
2018-06-07 10:30:53 +0200

    webpack + babel config

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App';
+
+const app = ( 
+	<App />
+);
+
+ReactDOM.render(app, document.getElementById('root'));
